4753b78fe7eef1a464a1a4954ec8a01c70803c01
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-03-19 12:02:51 +0200

    error handling

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,33 @@ const axios = require("axios");
 const route = "http://localhost:3001/api/";
 function App() {
   const [input, setInput] = useState("");
-  const pokemon = {
+  const [pokemon, setPokemon] = useState({
     name: "",
     weight: "",
     height: "",
     types: [],
-  };
+  });
+  let [validate, isValid] = useState(true);
   function changePokemon(input) {
     axios
       .get(route + "pokemon/" + input)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      .then((res) => {
+        setPokemon(res.data);
+        isValid(true);
+      })
+      .catch((err) => isValid(false));
   }
   function changeInput(newInput) {
     setInput(newInput);
-    console.log(input);
   }
   return (
     <div>
       <h1>Pokedex</h1>
-      <Search handler={() => changePokemon(input)} changeInput={changeInput} />
+      <Search
+        handler={() => changePokemon(input)}
+        changeInput={changeInput}
+        valid={validate}
+      />
       <Info pokemon={pokemon} />
     </div>
   );
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,5 +1,6 @@
 const Search = (props) => (
   <span className="search-span">
+    <p>{props.valid === false ? "Pokemon not found" : ""}</p>
     <input onChange={(e) => props.changeInput(e.target.value)} type="text" />
     <button onClick={props.handler}>search pokemon</button>
   </span>
